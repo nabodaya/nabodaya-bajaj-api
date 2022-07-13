0707c3488d1c48f582360b86ddc4f3e57ac97917
nabodaya <109249154+nabodaya@users.noreply.github.com>
2022-07-14 03:25:03 +0530

    Add files via upload

--- challenge.js
+++ challenge.js
@@ -1,29 +1,7 @@
-function charIsLetter(char) {
-  if (typeof char !== "string") {
-    return false;
-  }
+const express = require("express");
+const router = express.Router();
+const challengeController = require("../Controllers/challenge");
 
-  return /^[a-zA-Z]+$/.test(char);
-}
+router.post("/challenge", challengeController.getData);
 
-exports.getData = async (req, res, next) => {
-  try {
-    data = await req.body;
-    const numberArray = data.data.filter((item) => +item);
-    const alphabetArray = data.data.filter((item) => charIsLetter(item));
-    const totalCount = numberArray.length + alphabetArray.length;
-    res.status(200).json({
-      is_success: true,
-      user_id: "Nabodaya_Nayak_18121999",
-      count: totalCount.toString(),
-      email: "nabodaya.nayak.btech2019@sitpune.edu.in",
-      roll_number: "19070123067",
-      numbers: numberArray,
-      alphabets: alphabetArray,
-    });
-  } catch (error) {
-    res.json({ is_success: false });
-    error.statusCode = 500;
-    throw error;
-  }
-};
+module.exports = router;
